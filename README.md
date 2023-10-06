252437fe187a030f3c6f9b868ccd61e664b3f7ee
devMachadoA <amachadoamaral2@gmail.com>
2023-10-06 11:07:28 +0100

    interactive

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafios do Frontend Mentor - JS Newbie
+
+Este repositório contém desafios da seção gratuita do Frontend Mentor para iniciantes em JavaScript (JS newbie), retirados do site https://www.frontendmentor.io
--- interactive-rating-component-main/JS/result.js
+++ interactive-rating-component-main/JS/result.js
@@ -1,6 +1,4 @@
 const result = document.getElementById('result');
-const queryString = window.location.search;
-const params = new URLSearchParams(queryString);
-const valor = params.get('valor');
+const valor = new URLSearchParams(window.location.search).get('valor')
 
 result.innerHTML = `You selected ${valor} out of 5 `;
