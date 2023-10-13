b3a428a687860ad702987e6600a257c3c541952a
devMachadoA <amachadoamaral2@gmail.com>
2023-10-13 20:00:49 +0100

    js modificado

--- faq-accordion-card-main/js/script.js
+++ faq-accordion-card-main/js/script.js
@@ -1,20 +1,24 @@
 const arrows = document.querySelectorAll('.arrow');
+const questions = document.querySelectorAll('.question');
 const answers = document.querySelectorAll('.answer');
 const titles = document.querySelectorAll('.question h2');
 const element = document.querySelector('.container');
-const heightFixed = 495;
+let heightFixed = 495;
+let heightContainer
 let heightTotal = heightFixed;
 let openIndex = null;
 
-arrows.forEach((arrow, index) => {
-    arrow.addEventListener('click', () => {
-        arrow.classList.toggle('arrow-active');
+questions.forEach((question, index) => {
+    question.addEventListener('click', () => {
+        arrows[index].classList.toggle('arrow-active');
         answers[index].classList.toggle('answer-active');
         titles[index].classList.toggle('active-h2');
 
         if (openIndex !== null && openIndex !== index) {
-            arrow.classList.remove('arrow-active');
+            question.classList.remove('arrow-active');
             answers[openIndex].classList.remove('answer-active');
+            titles[openIndex].classList.remove('active-h2');
+            arrows[openIndex].classList.remove('arrow-active');
         }
 
         heightTotal = heightFixed + (answers[index].classList.contains('answer-active') ? answers[index].offsetHeight : 0);
@@ -22,4 +26,23 @@ arrows.forEach((arrow, index) => {
         openIndex = (openIndex === index) ? null : index;
     });
 });
+
+
+
+// arrows.forEach((arrow, index) => {
+//     arrow.addEventListener('click', () => {
+//         arrow.classList.toggle('arrow-active');
+//         answers[index].classList.toggle('answer-active');
+//         titles[index].classList.toggle('active-h2');
+
+//         if (openIndex !== null && openIndex !== index) {
+//             arrow.classList.remove('arrow-active');
+//             answers[openIndex].classList.remove('answer-active');
+//         }
+
+//         heightTotal = heightFixed + (answers[index].classList.contains('answer-active') ? answers[index].offsetHeight : 0);
+//         element.style.height = heightTotal + 'px';
+//         openIndex = (openIndex === index) ? null : index;
+//     });
+// });
 
