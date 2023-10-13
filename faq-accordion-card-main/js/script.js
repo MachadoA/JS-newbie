3d81ef657f2b9c7bf246d2e20c4d175040e2d58e
devMachadoA <amachadoamaral2@gmail.com>
2023-10-13 20:44:25 +0100

    css modified

--- faq-accordion-card-main/js/script.js
+++ faq-accordion-card-main/js/script.js
@@ -4,12 +4,13 @@ const answers = document.querySelectorAll('.answer');
 const titles = document.querySelectorAll('.question h2');
 const element = document.querySelector('.container');
 let heightFixed = 495;
-let heightContainer
 let heightTotal = heightFixed;
 let openIndex = null;
 
 questions.forEach((question, index) => {
     question.addEventListener('click', () => {
+        let screenWidth = window.innerWidth; 
+        console.log(screenWidth);
         arrows[index].classList.toggle('arrow-active');
         answers[index].classList.toggle('answer-active');
         titles[index].classList.toggle('active-h2');
@@ -21,28 +22,15 @@ questions.forEach((question, index) => {
             arrows[openIndex].classList.remove('arrow-active');
         }
 
-        heightTotal = heightFixed + (answers[index].classList.contains('answer-active') ? answers[index].offsetHeight : 0);
+        if (screenWidth <= 950) {
+            heightTotal = heightFixed + (answers[index].classList.contains('answer-active') ? answers[index].offsetHeight : 0);
+        } else {
+            heightTotal = heightFixed;
+        }
+
         element.style.height = heightTotal + 'px';
         openIndex = (openIndex === index) ? null : index;
     });
 });
 
-
-
-// arrows.forEach((arrow, index) => {
-//     arrow.addEventListener('click', () => {
-//         arrow.classList.toggle('arrow-active');
-//         answers[index].classList.toggle('answer-active');
-//         titles[index].classList.toggle('active-h2');
-
-//         if (openIndex !== null && openIndex !== index) {
-//             arrow.classList.remove('arrow-active');
-//             answers[openIndex].classList.remove('answer-active');
-//         }
-
-//         heightTotal = heightFixed + (answers[index].classList.contains('answer-active') ? answers[index].offsetHeight : 0);
-//         element.style.height = heightTotal + 'px';
-//         openIndex = (openIndex === index) ? null : index;
-//     });
-// });
 
