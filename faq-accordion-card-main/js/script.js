d08df8cbe53ba2429d0d3c34cbc6c89fa0341eeb
devMachadoA <amachadoamaral2@gmail.com>
2023-10-13 18:40:33 +0100

    version first mobile

--- faq-accordion-card-main/js/script.js
+++ faq-accordion-card-main/js/script.js
@@ -1,12 +1,25 @@
 const arrows = document.querySelectorAll('.arrow');
 const answers = document.querySelectorAll('.answer');
 const titles = document.querySelectorAll('.question h2');
+const element = document.querySelector('.container');
+const heightFixed = 495;
+let heightTotal = heightFixed;
+let openIndex = null;
 
-arrows.forEach(function(arrow, index){
-    arrow.addEventListener('click', () =>{
+arrows.forEach((arrow, index) => {
+    arrow.addEventListener('click', () => {
         arrow.classList.toggle('arrow-active');
         answers[index].classList.toggle('answer-active');
         titles[index].classList.toggle('active-h2');
-        console.log(titles[index])
-        })
-    })
+
+        if (openIndex !== null && openIndex !== index) {
+            arrow.classList.remove('arrow-active');
+            answers[openIndex].classList.remove('answer-active');
+        }
+
+        heightTotal = heightFixed + (answers[index].classList.contains('answer-active') ? answers[index].offsetHeight : 0);
+        element.style.height = heightTotal + 'px';
+        openIndex = (openIndex === index) ? null : index;
+    });
+});
+
